ceea2f3a80045a9f6db345d7ff9ff80afaa4dd43
Tracker Parser <info@trackerparser.com>
2025-09-30 07:08:26 +0000

    no nada, creo esto dejo de funcionar luego del ajuste del sidebar, pero

--- src/app/dashboard/plans/page.tsx
+++ src/app/dashboard/plans/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { PageHeader } from "@/components/page-header";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
