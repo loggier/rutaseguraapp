060eba122d37c9674800f0557618159566e7f4c5
Tracker Parser <info@trackerparser.com>
2025-10-09 17:53:46 +0000

    oye ahora dañaste no me puedo loguear, ahora no me lleva al dash como an

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,34 @@
-import { type NextRequest, NextResponse } from 'next/server'
+'use server';
+
+import { NextResponse, type NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const userCookie = request.cookies.get('rutasegura_user');
   const { pathname } = request.nextUrl;
 
-  // Si el usuario no está autenticado (no hay cookie) y intenta acceder a cualquier ruta del dashboard,
-  // redirígelo a la página de login.
+  // Si no hay cookie y se intenta acceder al dashboard, redirigir a login.
   if (!userCookie && pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
-  // Si el usuario está autenticado (hay cookie) y está en la página de login,
-  // redirígelo al dashboard.
+  // Si hay cookie y se está en la página de login, redirigir al dashboard.
   if (userCookie && pathname === '/') {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
-  // Si no se cumple ninguna de las condiciones anteriores, permite que la solicitud continúe.
   return NextResponse.next();
 }
 
 export const config = {
+  /*
+  * Match all request paths except for the ones starting with:
+  * - api (API routes)
+  * - _next/static (static files)
+  * - _next/image (image optimization files)
+  * - favicon.ico (favicon file)
+  * - image assets (logo, etc.)
+  */
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - api/ (API routes)
-     * - logo files
-     */
     '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
