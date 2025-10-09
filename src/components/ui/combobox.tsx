254eee33861547a42bc307658e5c04c9d56040ac
Tracker Parser <info@trackerparser.com>
2025-10-09 17:12:04 +0000

    porque poner un check si solo es un selector simple con buscador mas nad

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -1,10 +1,10 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import { Check, ChevronsUpDown } from "lucide-react"
+import * as React from 'react';
+import { ChevronsUpDown } from 'lucide-react';
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 import {
   Command,
   CommandEmpty,
@@ -12,12 +12,12 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/components/ui/command"
+} from '@/components/ui/command';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from '@/components/ui/popover';
 
 type ComboboxProps = {
     items: { value: string; label: string }[];
@@ -29,7 +29,7 @@ type ComboboxProps = {
 }
 
 export function Combobox({ items, value, onChange, placeholder, searchPlaceholder, notFoundMessage }: ComboboxProps) {
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -57,16 +57,10 @@ export function Combobox({ items, value, onChange, placeholder, searchPlaceholde
                   key={item.value}
                   value={item.label} // Filtering is done by label
                   onSelect={() => {
-                    onChange(item.value);
+                    onChange(item.value === value ? '' : item.value);
                     setOpen(false);
                   }}
                 >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === item.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
                   {item.label}
                 </CommandItem>
               ))}
@@ -75,5 +69,5 @@ export function Combobox({ items, value, onChange, placeholder, searchPlaceholde
         </Command>
       </PopoverContent>
     </Popover>
-  )
+  );
 }
