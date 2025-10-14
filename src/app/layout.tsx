cafc94003545d4fe23e2378a15e3127371debc56
Tracker Parser <info@trackerparser.com>
2025-10-14 06:04:48 +0000

    Try fixing this error: `Console Error: You have included the Google Maps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "RutaSegura",
