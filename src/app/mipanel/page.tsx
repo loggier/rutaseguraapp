c7d28b4b98ca23db6335f0a39b5835a5d5bcf13c
Tracker Parser <info@trackerparser.com>
2025-10-28 07:15:07 +0000

    noooo, porque lo dañaste si habia quedado perfecto

--- src/app/mipanel/page.tsx
+++ src/app/mipanel/page.tsx
@@ -190,43 +190,44 @@ export default function MiPanelPage() {
                     const shadowOffset = 2;
 
                     const bubbleSize = isActive ? activeSize : baseSize;
-                    
-                    const bubbleWidth = bubbleSize + shadowOffset * 2;
-                    const bubbleHeight = bubbleSize + pinHeight + shadowOffset * 2;
+                    const totalWidth = bubbleSize + (borderWidth * 2) + (shadowOffset * 2);
+                    const totalHeight = bubbleSize + (borderWidth * 2) + pinHeight + (shadowOffset * 2);
+                    const centerX = totalWidth / 2;
+                    const centerY = (bubbleSize + (borderWidth * 2)) / 2;
+
                     const borderColor = isActive ? '#01C998' : '#A1A1AA';
 
-                     const bubbleSvg = `
-                        <svg width="${bubbleWidth}" height="${bubbleHeight}" viewBox="0 0 ${bubbleWidth} ${bubbleHeight}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                            <defs>
-                                <filter id="shadow" x="0" y="0" width="${bubbleWidth}" height="${bubbleHeight}" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                                    <feOffset dy="1"/>
-                                    <feGaussianBlur stdDeviation="1.5"/>
-                                    <feComposite in2="hardAlpha" operator="out"/>
-                                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
-                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
-                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
-                                </filter>
-                            </defs>
-                            <g filter="url(#shadow)">
-                                <path d="M ${bubbleWidth / 2} ${bubbleSize + pinHeight} L ${bubbleWidth / 2 - pinHeight / 1.5} ${bubbleSize} H ${bubbleWidth / 2 + pinHeight / 1.5} Z" fill="${borderColor}" />
-                                <circle cx="${bubbleWidth / 2}" cy="${bubbleSize / 2}" r="${bubbleSize / 2}" fill="${borderColor}"/>
-                                <circle cx="${bubbleWidth / 2}" cy="${bubbleSize / 2}" r="${(bubbleSize - borderWidth) / 2}" fill="white"/>
-                            </g>
-                        </svg>`.trim();
+                    const bubbleSvg = `
+                      <svg width="${totalWidth}" height="${totalHeight}" viewBox="0 0 ${totalWidth} ${totalHeight}" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                          <filter id="shadow" x="0" y="0" width="${totalWidth}" height="${totalHeight}" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                            <feOffset dy="1"/>
+                            <feGaussianBlur stdDeviation="1.5"/>
+                            <feComposite in2="hardAlpha" operator="out"/>
+                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
+                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+                          </filter>
+                        </defs>
+                        <g filter="url(#shadow)">
+                          <path d="M ${centerX} ${centerY + (bubbleSize / 2) + pinHeight} L ${centerX - (pinHeight / 1.5)} ${centerY + (bubbleSize / 2)} H ${centerX + (pinHeight / 1.5)} Z" fill="${borderColor}" />
+                          <circle cx="${centerX}" cy="${centerY}" r="${bubbleSize / 2 + borderWidth}" fill="${borderColor}"/>
+                          <circle cx="${centerX}" cy="${centerY}" r="${bubbleSize / 2}" fill="white"/>
+                        </g>
+                      </svg>`.trim();
 
-                    const avatarSize = bubbleSize - (borderWidth * 2);
-                    const avatarAnchorY = bubbleHeight - shadowOffset - (bubbleSize - avatarSize) / 2 - avatarSize / 2;
-
+                    const avatarSize = bubbleSize;
+                    
                     markers.push(
                         <React.Fragment key={`${hijo.id}-marker-group`}>
                             <MarkerF
                                 position={position}
                                 icon={{
                                     url: `data:image/svg+xml;base64,${btoa(bubbleSvg)}`,
-                                    scaledSize: new google.maps.Size(bubbleWidth, bubbleHeight),
-                                    anchor: new google.maps.Point(bubbleWidth / 2, bubbleHeight - shadowOffset),
+                                    scaledSize: new google.maps.Size(totalWidth, totalHeight),
+                                    anchor: new google.maps.Point(centerX, totalHeight - shadowOffset),
                                 }}
                                 zIndex={isActive ? 95 : 90}
                                 onClick={() => {
@@ -238,10 +239,10 @@ export default function MiPanelPage() {
                                 icon={{
                                     url: hijo.avatar_url,
                                     scaledSize: new google.maps.Size(avatarSize, avatarSize),
-                                    anchor: new google.maps.Point(bubbleWidth / 2, avatarAnchorY),
+                                    anchor: new google.maps.Point(centerX, centerY),
                                 }}
                                 shape={{
-                                    coords: [bubbleWidth/2, avatarAnchorY, avatarSize/2],
+                                    coords: [centerX, centerY, avatarSize/2],
                                     type: 'circle'
                                 }}
                                 zIndex={isActive ? 96 : 91}
@@ -411,7 +412,3 @@ export default function MiPanelPage() {
         </div>
     );
 }
-
-    
-
-    
